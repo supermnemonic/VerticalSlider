8d0576b44f7acfebc29602441ea1ddd0e8a427eb
Muhammad Alam Akbar <supermnemonic@users.noreply.github.com>
2017-12-07 18:43:30 +0700

    custom trackRect height

--- VerticalSlider/Classes/CustomSlider.swift
+++ VerticalSlider/Classes/CustomSlider.swift
@@ -10,17 +10,18 @@ import UIKit
 
 open class CustomSlider: UISlider {
     
-let trackHeight:CGFloat = 28
+    open static var trackHeight:CGFloat = 12
     
 //    @IBInspectable var height: CGFloat = 100
-//    override public func trackRect(forBounds bounds: CGRect) -> CGRect {
-//        let defaultBounds = super.trackRect(forBounds: bounds)
-//        return CGRect(
-//            x: defaultBounds.origin.x,
-//            y: defaultBounds.origin.y + defaultBounds.size.height/2 - trackHeight/2,
-//            width: defaultBounds.size.width,
-//            height: trackHeight
-//        )
-//    }
+    
+    override open func trackRect(forBounds bounds: CGRect) -> CGRect {
+        let defaultBounds = super.trackRect(forBounds: bounds)
+        return CGRect(
+            x: defaultBounds.origin.x,
+            y: defaultBounds.origin.y + defaultBounds.size.height/2 - CustomSlider.trackHeight/2,
+            width: defaultBounds.size.width,
+            height: CustomSlider.trackHeight
+        )
+    }
     
 }
